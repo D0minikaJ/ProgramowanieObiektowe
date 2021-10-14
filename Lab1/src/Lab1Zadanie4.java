054176a223b35fe4a7bd9c826a68347689b1c73a
DominikaJ <dominikaj@email.com>
2021-10-14 21:19:16 +0200

    message

---
+++ Lab1/src/Lab1Zadanie4.java
@@ -0,0 +1,12 @@
+public class Zadanie4 {
+    public static void main(String[] args){
+        int x = 1000;
+        double y = 1.06;
+        double rok1 = x*y;
+        double rok2 = rok1*y;
+        double rok3 = rok2*y;
+        System.out.println(rok1);
+        System.out.println(rok2);
+        System.out.println(rok3);
+    }
+}
